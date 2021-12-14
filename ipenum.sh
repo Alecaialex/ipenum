345da828733767a76049f886943fc895a25ac837
Alejandro Sanz <46007072+Alecaialex@users.noreply.github.com>
2021-12-14 12:39:07 -0500

    ipenum.sh

---
+++ ipenum.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ip=${1:-$1}
+
+for complete in $(seq 1 255); do
+    ttlstr=$(ping -c1 -w1 $ip.$complete | grep -o 'ttl=[0-9][0-9]*') || {
+        continue;
+    }
+    ttl="${ttlstr#*=}"
+    printf "%s está online, ttl=%d\n" "$ip.$complete" "$ttl"
+done
